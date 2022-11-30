70bb63d39ec7376b4b3f208e57dacd63a007d183
jimdmm <jimdouglasmmferreira@gmail.com>
2022-11-30 19:15:37 -0300

    #Finalização e restruturação do código

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
 // Função TocaSom com o parâmetro idElementoAudio
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-    }
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  // Comparação de elemento com o operador AND para dar play
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  }
+  else {
+    console.log("Elemento não encontrado ou seletor inválido");
+  }
+}
 
 // Criação de uma array apartir de uma querySelectorALL
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
 // Criação da estrutura de repetição para percorrer todas as teclas
 
 for (let i = 0; i < listaDeTeclas.length; i++) {
+  const tecla = listaDeTeclas[i];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`; //Template string
 
-    const tecla =  listaDeTeclas[i];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`;
+  // Com o evento onclick será criado uma arrow function para chamar o TocaSom que irá receber o idAudio
+  tecla.onclick = () => {
+    tocaSom(idAudio);
+  };
 
-// Com o evento onclick será criado uma arrow function para chamar o TocaSom que irá receber o idAudio
-    tecla.onclick = () =>{
-        tocaSom(idAudio);
+  // Comparação de evento com o operador OR para evitar repetição de código
+  tecla.onkeydown = (e) => {
+    if (e.code === "Space" || e.code === "Enter") {
+      tecla.classList.add("ativa");
     }
+  };
 
-    tecla.onkeydown = (e) => {
-
-        if (e.code === 'Space') {
-            tecla.classList.add('ativa');
-        }
-    }
-
-    tecla.onkeyup = () => {
-        tecla.classList.remove('ativa');
-    }
-
+  tecla.onkeyup = () => {
+    tecla.classList.remove("ativa");
+  };
 }
